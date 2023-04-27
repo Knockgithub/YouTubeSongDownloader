129186aa854f85d0c9330e82eff90eff40074f39
Knockgithub <131382142+Knockgithub@users.noreply.github.com>
2023-04-27 16:16:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "27535343"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "06766128d68d9c1a0d683ecfbb91de2f" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "6024489085:AAEajd4l5OTAr6LbscEWvpnS1Z6-oNAzhfo" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
